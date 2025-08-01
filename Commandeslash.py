3cc1dc9ba3f5164862aeadb1e0527935a1bf6e36
AlphaGames30 <edonis.imbert@gmail.com>
2025-08-01 14:03:23 +0200

    Update Commandeslash.py

--- Commandeslash.py
+++ Commandeslash.py
@@ -13,7 +13,30 @@ async def multiplication(interaction: discord.Interaction, nombre1: int, nombre2
 bot.tree.command(name="youtube" , description=  "envoie un lien vers la chaine youtube alpha_mine0285")
 async def youtube(interaction: discord.Interaction):
   await interaction.response.send_message("https://youtube.com/@play-zrfix.fortblilx?si=7rXbE_3NwV0SI3ZP")
-  
+
+bot.tree.command(name="ticketsetup", description= "envoie un message pour crée un ticket")
+async def ticketsetup(interaction: discord.Interaction):
+  await interaction.response.send_message("Cliquez sur le bouton pour crée un ticket")
+  await interaction.response.send_message(view=TicketView())
+  await interaction.response.send_message("ticket crée")
+
+class TicketView(discord.ui.View):
+  @discord.ui.button(label="Créer un ticket",
+style=discord.ButtonStyle.primary,
+emoji="🎫")
+  async def ticket(self, interaction: discord.Interaction, button:
+  discord.ui.Button):
+    await interaction.response.send_message("ticket crée", ephemeral=True)
+    await interaction.user.send("votre ticket a était crée !")
+    await interaction.user.send(view=CloseTicketView())
+
+class CloseTicketView(discord.ui.View):
+  @discord.ui.button(label="Fermé le ticket",
+style=discord.ButtonStyle.primary,
+emoji="🔒")
+  async def close_ticket(self, interaction: discord.Interaction, button):
+   await interaction.response.send_message("ticket férmé", ephemeral=True)
+
 @bot.event
 async def on_ready():
   print(f' connécté en tant que {bot.user}')
