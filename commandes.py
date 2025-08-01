9ea18527139442323636f535143e22abaa47c811
AlphaGames30 <edonis.imbert@gmail.com>
2025-08-01 17:37:24 +0200

    Update commandes.py

--- commandes.py
+++ commandes.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-token = 'MTM0NTM0Mjg5MjUwMzIwNzk5Nw.GbDbdL.u4iWvc61OCM1IqN73Bv6d4Ue9X_byFgHd3y_f0'
+token = 'DISCORD_TOKEN'
 
 intents = discord.Intents.all()
 
