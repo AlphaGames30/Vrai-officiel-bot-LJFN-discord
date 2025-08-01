45c698a7a873d117204b41ce6b495aecd1b4f78e
AlphaGames30 <edonis.imbert@gmail.com>
2025-08-01 12:46:21 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import discord
 
-token = "MTM0NTM0Mjg5MjUwMzIwNzk5Nw.GbDbdL.u4iWvc61OCM1IqN73Bv6d4Ue9X_byFgHd3y_f0"
+token = "MTM0NTM0Mjg5MjUwMzIwNzk5Nw.GrRxdB.cnNTwEeh1ZkupgfyB74bRRoux51gwKKguwF9tQ"
 
 client = discord.Client(intents=discord.Intents.all())
 
