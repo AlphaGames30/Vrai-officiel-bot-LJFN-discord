abc57136d275ff076a60512aad2c3043bd5261f9
AlphaGames30 <edonis.imbert@gmail.com>
2025-08-01 12:48:09 +0200

    Update start.py

--- start.py
+++ start.py
@@ -16,6 +16,13 @@ client = discord.Client(intents=discord.Intents.all())
                                
 intents = discord.Intents.all()
 bot = MonBot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+  await bot.tree.sync()
+  print(f'connété en tant que {bot.user}')
+
 
 keep_alive()
 bot.run(token='DISCORD_TOKEN') 
